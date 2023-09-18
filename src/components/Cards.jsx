f8a7fddcc5b1a5d1aaef5a59d1fe1ba796b01869
César Silvestre Ccasani Huamán <126905785+razuwilca@users.noreply.github.com>
2023-09-18 00:51:05 -0500

    update link

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -12,9 +12,9 @@ const imagenes = {
 
 const linkDrive = {
   primero:
-    "https://docs.google.com/forms/d/e/1FAIpQLSf0wxEeQmnDjrJWJxwQ2hQOCifa9WsABbw_u-Tr1UAolwLccw/viewform?usp=",
+    "https://docs.google.com",
   segundo:
-    "https://docs.google.com/forms/d/e/1FAIpQLSdp6Fan0hVDYoZp6JLdbplQ_eDAXS42kc8Vln-RDHANSVBJSA/viewform?usp=s",
+    "https://docs.google.com",
   tercero:
     "https://drive.google.com/drive/folders/1y70yKIRNcE5-otLJy17yxPRliaaXLJuX?usp=sharing",
   cuarto:
